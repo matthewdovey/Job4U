90e57ffa2904907c992ce7fbdd75a7e32e8e9738
matthew dovey <matthewdovey@hotmail.co.uk>
2019-01-14 20:18:37 +0000

    feat: rename of view controller to login view controller

--- Job4U/LoginViewController.m
+++ Job4U/LoginViewController.m
@@ -6,13 +6,13 @@
 //  Copyright © 2019 Matthew Dovey. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "LoginViewController.h"
 
-@interface ViewController ()
+@interface LoginViewController ()
 
 @end
 
-@implementation ViewController
+@implementation LoginViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
