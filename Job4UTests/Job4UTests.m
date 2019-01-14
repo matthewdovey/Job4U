c8ada033b7ffce652ca511341f5d28faf27f69c6
matthew dovey <matthewdovey@hotmail.co.uk>
2019-01-14 19:53:02 +0000

    Initial Commit

---
+++ Job4UTests/Job4UTests.m
@@ -0,0 +1,37 @@
+//
+//  Job4UTests.m
+//  Job4UTests
+//
+//  Created by Matthew Dovey on 14/01/2019.
+//  Copyright © 2019 Matthew Dovey. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Job4UTests : XCTestCase
+
+@end
+
+@implementation Job4UTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
