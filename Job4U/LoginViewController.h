e61dfa724232d8aed3441a706a79ad613af70f1a
matthew dovey <matthewdovey@hotmail.co.uk>
2019-01-14 20:19:31 +0000

    docs: update of view controller docs to reflect rename

--- Job4U/LoginViewController.h
+++ Job4U/LoginViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  LoginViewController.h
 //  Job4U
 //
 //  Created by Matthew Dovey on 14/01/2019.
--- Job4U/LoginViewController.m
+++ Job4U/LoginViewController.m
@@ -1,5 +1,5 @@
 //
-//  ViewController.m
+//  LoginViewController.m
 //  Job4U
 //
 //  Created by Matthew Dovey on 14/01/2019.
