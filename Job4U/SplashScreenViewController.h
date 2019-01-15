5e0c31a7c7ff113aa10ce43b2b5654c89519ed98
matthew dovey <matthewdovey@hotmail.co.uk>
2019-01-15 13:28:35 +0000

    feat: addition of splash screen coordinator and view controllers

---
+++ Job4U/SplashScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  SplashScreenViewController.h
+//  Job4U
+//
+//  Created by Matthew Dovey on 15/01/2019.
+//  Copyright © 2019 Matthew Dovey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SplashScreenViewController : UIViewController
+
+@end
