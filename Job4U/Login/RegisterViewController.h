92bff2ff1929ede820cb7b7d65cd7a94087c2c42
matthew dovey <matthewdovey@hotmail.co.uk>
2019-01-15 12:16:18 +0000

    feat: creation and initial setup of register view controller

---
+++ Job4U/Login/RegisterViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegisterViewController.h
+//  Job4U
+//
+//  Created by Matthew Dovey on 15/01/2019.
+//  Copyright © 2019 Matthew Dovey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegisterViewController : UIViewController
+
+@end
---
+++ Job4U/Login/RegisterViewController.m
@@ -0,0 +1,23 @@
+//
+//  RegisterViewController.m
+//  Job4U
+//
+//  Created by Matthew Dovey on 15/01/2019.
+//  Copyright © 2019 Matthew Dovey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "RegisterViewController.h"
+
+@interface RegisterViewController ()
+
+@end
+
+@implementation RegisterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
