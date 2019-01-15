b0e9391097ed3776ab82d516aa2746e04ac5ce3c
matthew dovey <matthewdovey@hotmail.co.uk>
2019-01-15 11:27:44 +0000

    feat: addition of categories group and ui font category header and implementation

---
+++ Job4U/Categories/UIFont+CustomFonts.h
@@ -0,0 +1,13 @@
+//
+//  UIFont+CustomFonts.h
+//  Job4U
+//
+//  Created by Matthew Dovey on 15/01/2019.
+//  Copyright © 2019 Matthew Dovey. All rights reserved.
+//
+
+#ifndef UIFont_CustomFonts_h
+#define UIFont_CustomFonts_h
+
+
+#endif /* UIFont_CustomFonts_h */
