10731e69ddcfb1c14bd4974765dbc68c2236c9e0
matthew dovey <matthewdovey@hotmail.co.uk>
2019-01-15 20:36:27 +0000

    feat: initial setup of dashboard coordinator

---
+++ Job4U/Coordinator/DashboardCoordinator.h
@@ -0,0 +1,13 @@
+//
+//  DashboardCoordinator.h
+//  Job4U
+//
+//  Created by Matthew Dovey on 15/01/2019.
+//  Copyright © 2019 Matthew Dovey. All rights reserved.
+//
+
+#import "Coordinator.h"
+
+@interface DashboardCoordinator : NSObject <Coordinator>
+
+@end
