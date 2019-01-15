7bc5b3bd3f954c87abd7e20e65104d8a9831b2e2
matthew dovey <matthewdovey@hotmail.co.uk>
2019-01-15 20:26:24 +0000

    feat: addition of dashboard group with view controller and view model creation

---
+++ Job4U/Dashboard/DashboardViewController.h
@@ -0,0 +1,13 @@
+//
+//  DashboardViewController.h
+//  Job4U
+//
+//  Created by Matthew Dovey on 15/01/2019.
+//  Copyright © 2019 Matthew Dovey. All rights reserved.
+//
+
+#ifndef DashboardViewController_h
+#define DashboardViewController_h
+
+
+#endif /* DashboardViewController_h */
---
+++ Job4U/Dashboard/DashboardViewModel.h
@@ -0,0 +1,13 @@
+//
+//  DashboardViewModel.h
+//  Job4U
+//
+//  Created by Matthew Dovey on 15/01/2019.
+//  Copyright © 2019 Matthew Dovey. All rights reserved.
+//
+
+#ifndef DashboardViewModel_h
+#define DashboardViewModel_h
+
+
+#endif /* DashboardViewModel_h */
