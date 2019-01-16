fe181619c14b412f58e93836b15e8692ba254a2e
matthew dovey <matthewdovey@hotmail.co.uk>
2019-01-16 21:26:51 +0000

    feat: setup of dashboard view controller ui

--- Job4U/Dashboard/DashboardViewController.m
+++ Job4U/Dashboard/DashboardViewController.m
@@ -8,13 +8,70 @@
 
 #import <Foundation/Foundation.h>
 #import "DashboardViewController.h"
+#import "DashboardViewModel.h"
 
 @interface DashboardViewController ()
+
+@property (nonatomic, strong) UIView *topView;
 
 @end
 
 @implementation DashboardViewController
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
 
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    self.view.backgroundColor = [UIColor blueColor];
+}
+
+- (void)uiSetup {
+    _topView = [[UIView alloc] init];
+    [self.view addSubview:_topView];
+    _topView.backgroundColor = [UIColor redColor];
+    
+}
+
+- (void)constraintSetup {
+    NSLayoutConstraint *topConstraint = [NSLayoutConstraint
+                                         constraintWithItem:_topView
+                                         attribute:NSLayoutAttributeTop
+                                         relatedBy:NSLayoutRelationEqual
+                                         toItem:self.view
+                                         attribute:NSLayoutAttributeTop
+                                         multiplier:1.0f
+                                         constant:0];
+    NSLayoutConstraint *leadingConstraint = [NSLayoutConstraint
+                                             constraintWithItem:_topView
+                                             attribute:NSLayoutAttributeLeading
+                                             relatedBy:NSLayoutRelationEqual
+                                             toItem:self.view
+                                             attribute:NSLayoutAttributeLeading
+                                             multiplier:1.0f
+                                             constant:0];
+    NSLayoutConstraint *trailingConstraint = [NSLayoutConstraint
+                                              constraintWithItem:_topView
+                                              attribute:NSLayoutAttributeTrailing
+                                              relatedBy:NSLayoutRelationEqual
+                                              toItem:self.view
+                                              attribute:NSLayoutAttributeTrailing
+                                              multiplier:1.0f
+                                              constant:0];
+    NSLayoutConstraint *heightConstraint = [NSLayoutConstraint
+                                            constraintWithItem:_topView
+                                            attribute:NSLayoutAttributeHeight
+                                            relatedBy:NSLayoutRelationEqual
+                                            toItem:self.view
+                                            attribute:NSLayoutAttributeHeight
+                                            multiplier:1.0f
+                                            constant:0];
+    
+    [self.view addConstraint:topConstraint];
+    [self.view addConstraint:leadingConstraint];
+    [self.view addConstraint:trailingConstraint];
+    [self.view addConstraint:heightConstraint];
+}
 
 @end
