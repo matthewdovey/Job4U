66a14cb9aa6e683e2cf1a3e1ae042ab8c1f6167a
matthew dovey <matthewdovey@hotmail.co.uk>
2019-01-16 21:55:33 +0000

    feat: creation of login coordinator

---
+++ Job4U/Coordinator/LoginCoordinator.h
@@ -0,0 +1,13 @@
+//
+//  LoginCoordinator.h
+//  Job4U
+//
+//  Created by Matthew Dovey on 16/01/2019.
+//  Copyright © 2019 Matthew Dovey. All rights reserved.
+//
+
+#ifndef LoginCoordinator_h
+#define LoginCoordinator_h
+
+
+#endif /* LoginCoordinator_h */
---
+++ Job4U/Coordinator/LoginCoordinator.m
@@ -0,0 +1,9 @@
+//
+//  LoginCoordinator.m
+//  Job4U
+//
+//  Created by Matthew Dovey on 16/01/2019.
+//  Copyright © 2019 Matthew Dovey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
