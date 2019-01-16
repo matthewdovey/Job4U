aa5f1a7dc0483477484d7be96d6c3062a9dd9057
matthew dovey <matthewdovey@hotmail.co.uk>
2019-01-16 22:50:56 +0000

    feat: creation of sign in delegate

---
+++ Job4U/Login/SignIn/SignInDelegate.h
@@ -0,0 +1,13 @@
+//
+//  SignInDelegate.h
+//  Job4U
+//
+//  Created by Matthew Dovey on 16/01/2019.
+//  Copyright © 2019 Matthew Dovey. All rights reserved.
+//
+
+@protocol SignInDelegate <NSObject>
+
+- (void)successfulSignIn;
+
+@end
