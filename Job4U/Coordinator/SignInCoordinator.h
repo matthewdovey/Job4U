3f9b1afa97a7cd0421ab5a3e2f7ea0b6ad1a210f
matthew dovey <matthewdovey@hotmail.co.uk>
2019-01-18 22:02:19 +0000

    feat: initial creation and setup of sign in coordinator

---
+++ Job4U/Coordinator/SignInCoordinator.h
@@ -0,0 +1,15 @@
+//
+//  SignInCoordinator.h
+//  Job4U
+//
+//  Created by Matthew Dovey on 18/01/2019.
+//  Copyright © 2019 Matthew Dovey. All rights reserved.
+//
+
+#import "Coordinator.h"
+
+@interface SignInCoordinator : NSObject <Coordinator>
+
+- (id)initWithNavController:(UINavigationController*)navigationController;
+
+@end
