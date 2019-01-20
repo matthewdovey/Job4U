fffc57ad6f96040b09b0d69636af0ec4171ce691
matthew dovey <matthewdovey@hotmail.co.uk>
2019-01-20 16:36:38 +0000

    feat: creation and setup of register coordinator and delegate

---
+++ Job4U/Login/Register/RegisterDelegate.h
@@ -0,0 +1,13 @@
+//
+//  RegisterDelegate.h
+//  Job4U
+//
+//  Created by Matthew Dovey on 20/01/2019.
+//  Copyright © 2019 Matthew Dovey. All rights reserved.
+//
+
+@interface RegisterDelegate <NSObject>
+
+- (void)successfulRegister;
+
+@end
